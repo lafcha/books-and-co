c7b3ae9ecbfef0fa385472e9513be20130c4c0c2
JKawnN <auer.sylvain@gmail.com>
2021-04-29 16:00:27 +0200

    first controller

--- migrations/Version20210429134037.php
+++ migrations/Version20210429134037.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210429133059 extends AbstractMigration
+final class Version20210429134037 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,9 +20,14 @@ final class Version20210429133059 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE book ADD id INT AUTO_INCREMENT NOT NULL, CHANGE isbn isbn BIGINT NOT NULL, DROP PRIMARY KEY, ADD PRIMARY KEY (id)');
+        $this->addSql('CREATE TABLE book_users_book (book_id INT NOT NULL, users_book_id INT NOT NULL, INDEX IDX_9BC1A78C16A2B381 (book_id), INDEX IDX_9BC1A78CE6A2C74B (users_book_id), PRIMARY KEY(book_id, users_book_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user_users_book (user_id INT NOT NULL, users_book_id INT NOT NULL, INDEX IDX_A30B2BD3A76ED395 (user_id), INDEX IDX_A30B2BD3E6A2C74B (users_book_id), PRIMARY KEY(user_id, users_book_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE users_book (id INT AUTO_INCREMENT NOT NULL, users_book_is_lent_id INT NOT NULL, is_available TINYINT(1) NOT NULL, UNIQUE INDEX UNIQ_46CED3FE0F8B622 (users_book_is_lent_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE book_users_book ADD CONSTRAINT FK_9BC1A78C16A2B381 FOREIGN KEY (book_id) REFERENCES book (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE book_users_book ADD CONSTRAINT FK_9BC1A78CE6A2C74B FOREIGN KEY (users_book_id) REFERENCES users_book (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE user_users_book ADD CONSTRAINT FK_A30B2BD3A76ED395 FOREIGN KEY (user_id) REFERENCES `user` (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE user_users_book ADD CONSTRAINT FK_A30B2BD3E6A2C74B FOREIGN KEY (users_book_id) REFERENCES users_book (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE users_book ADD CONSTRAINT FK_46CED3FE0F8B622 FOREIGN KEY (users_book_is_lent_id) REFERENCES lending (id)');
         $this->addSql('ALTER TABLE lending ADD borrower_id INT NOT NULL');
         $this->addSql('ALTER TABLE lending ADD CONSTRAINT FK_74AB8C0311CE312B FOREIGN KEY (borrower_id) REFERENCES `user` (id)');
         $this->addSql('CREATE INDEX IDX_74AB8C0311CE312B ON lending (borrower_id)');
@@ -36,12 +41,11 @@ final class Version20210429133059 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE book MODIFY id INT NOT NULL');
-        $this->addSql('ALTER TABLE book DROP PRIMARY KEY');
-        $this->addSql('ALTER TABLE book DROP id, CHANGE isbn isbn BIGINT UNSIGNED NOT NULL');
-        $this->addSql('ALTER TABLE book ADD PRIMARY KEY (isbn)');
-        $this->addSql('ALTER TABLE book_users_book DROP FOREIGN KEY FK_9BC1A78C16A2B381');
         $this->addSql('ALTER TABLE book_users_book DROP FOREIGN KEY FK_9BC1A78CE6A2C74B');
+        $this->addSql('ALTER TABLE user_users_book DROP FOREIGN KEY FK_A30B2BD3E6A2C74B');
+        $this->addSql('DROP TABLE book_users_book');
+        $this->addSql('DROP TABLE user_users_book');
+        $this->addSql('DROP TABLE users_book');
         $this->addSql('ALTER TABLE lending DROP FOREIGN KEY FK_74AB8C0311CE312B');
         $this->addSql('DROP INDEX IDX_74AB8C0311CE312B ON lending');
         $this->addSql('ALTER TABLE lending DROP borrower_id');
