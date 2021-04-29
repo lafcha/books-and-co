1e21932f4e35acb6790285c3fd7ffc2ac583cde5
lafcha <charlotte.bronsard@gmail.com>
2021-04-29 17:46:11 +0200

    installation of tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+  theme: {
+    fontFamily: {
+     'serif': 'ui-serif',
+     'display': 'Lily Script One', cursive,
+     'body': 'Raleway',
+    }
+  }
+
+}
