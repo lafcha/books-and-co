bc34b79df505ddbe872840467522d913f12147ea
Ikimoo <peugeot.m.1904@gmail.com>
2021-05-04 17:39:06 +0200

    research feature done, still need some front

---
+++ src/Controller/SearchController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\SearchFormType;
+use App\Repository\BookRepository;
+use App\Repository\UserRepository;
+use App\Repository\UsersBookRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class SearchController extends AbstractController
+{
+    /**
+     * @Route("/recherche", name="search")
+     */
+    public function search(UsersBookRepository $usersBookRepository, Request $request): Response
+    {   
+        // For now, we want to show every book available in the department
+        // We're not searching with keywords !
+        $book = [];
+        $form = $this->createForm(SearchFormType::class, null, [
+            'method' => 'GET',
+        ]);
+        if($form->handleRequest($request)->isSubmitted() && $form->isValid()) {
+            $criteria = $form->getExtraData()['county'];
+            $book = $usersBookRepository->findAllAvalaibleBooksByCity($criteria);
+        }
+        dump($form);
+        dump($book);
+
+        return $this->render('search/search.html.twig', [
+            'form' => $form->createView(),
+            'book' => $book,
+        ]);
+    }
+
+}
