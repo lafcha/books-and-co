5ab569ca494fb60d817b907fedceb46087d0ba15
Ikimoo <peugeot.m.1904@gmail.com>
2021-05-11 10:31:55 +0200

    footer almost done

--- src/Controller/LibraryController.php
+++ src/Controller/LibraryController.php
@@ -23,7 +23,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 /**
  * @Route("/bibliotheque/{userSlug}", name="library_")
  */
-class LibraryController extends AbstractController
+class LibraryController extends MainController
 {
     /**
      * @Route("", name="browse")
@@ -31,7 +31,7 @@ class LibraryController extends AbstractController
     public function browse($userSlug, UsersBookRepository $usersBookRepository, UserRepository $userRepository, Request $request): Response
     {
         // set the limit of elements by page
-        $limit = 10;
+        $elementsLimit = 10;
         // get the page in url
         $page = (int)$request->query->get("page", 1);
         if ($page < 1) {
@@ -44,14 +44,14 @@ class LibraryController extends AbstractController
         }
         $userId = $user->getId();
         // get the count of usersBook
-        $usersBookTotal = (int)$usersBookRepository->getUsersBookById($userId);
+        $elementsTotal = (int)$usersBookRepository->getUsersBookById($userId);
 
 
         
         // find all books of a user with a $limit of element by page
-        $usersBooks = $usersBookRepository->findAllByUserId($userId, $page, $limit);
+        $usersBooks = $usersBookRepository->findAllByUserId($userId, $page, $elementsLimit);
 
-        if (empty($usersBooks) && $usersBookTotal != 0) {
+        if (empty($usersBooks) && $elementsTotal != 0) {
             // throw 404 if the page returns an empty array
             throw $this->createNotFoundException('Cette page n\'existe pas');
         }
@@ -59,9 +59,10 @@ class LibraryController extends AbstractController
         return $this->render('library/browse.html.twig', [
             'usersBooks' => $usersBooks,
             'currentPage' => $page,
-            'usersBookTotal' => $usersBookTotal,
-            'usersBookLimit' => $limit,
-            'user'=> $user,
+            'elementsTotal' => $elementsTotal,
+            'elementsLimit' => $elementsLimit,
+            'user' => $user,
+            'navSearchForm' => $this->navSearchForm()->createView(),
         ]);
     }
 
@@ -176,6 +177,7 @@ class LibraryController extends AbstractController
             'bookForm' => $bookForm->createView(),
             'libraryUser' => $libraryUser,
             'error' => $error,
+            'navSearchForm' => $this->navSearchForm()->createView(),
         ]);
     }
 
@@ -214,6 +216,7 @@ class LibraryController extends AbstractController
             'book' => $book,
             'libraryUser' => $libraryUser,
             'form' => $form->createView(),
+            'navSearchForm' => $this->navSearchForm()->createView(),
         ]);
     }
 
@@ -258,6 +261,7 @@ class LibraryController extends AbstractController
         return $this->render('library/book/edit.html.twig', [
             'book' => $book,
             'form' => $form->createView(),
+            'navSearchForm' => $this->navSearchForm()->createView(),
         ]);
     }
 
@@ -287,6 +291,7 @@ class LibraryController extends AbstractController
 
         return $this->redirectToRoute('library_browse', [
             'userSlug'=> $userSlug,
+            'navSearchForm' => $this->navSearchForm()->createView(),
         ]);
     }
 }
